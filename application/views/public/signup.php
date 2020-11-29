39acf053b04eb640716eb5ca0972bec2e90d6589
elisee <elisee.kalonji99@gmail.com>
2020-11-29 13:00:45 +0200

    js form validation

--- application/views/public/signup.php
+++ application/views/public/signup.php
@@ -19,32 +19,37 @@
                 </div>
 
                 <div class="login__block__body">
-                    <form action=<?=site_url('signinup/signup')?> method="post">
+                    <form action=<?=site_url('signinup/signup')?> method="post" id="form">
                         <div class="form-group form-group--float form-group--centered">
-                            <input type="text" class="form-control" name="nomcomplet">
+                            <input type="text" class="form-control" name="nomcomplet" id="nom">
                             <label>Nom complet</label>
                             <i class="form-group__bar"></i>
                         </div>                        
+                        <p><span id="namespan"></span></p>
 
                         <div class="form-group form-group--float form-group--centered">
-                            <input type="text" class="form-control" name="lieudeconsultation">
+                            <input type="text" class="form-control" name="lieudeconsultation" id="place">
                             <label>Lieu de consultation</label>
                             <i class="form-group__bar"></i>
                         </div>
+                        <p><span id="placespan"></span></p>
 
                         <div class="form-group form-group--float form-group--centered">
-                            <input type="text" class="form-control" name="username">
+                            <input type="text" class="form-control" name="username" id="username">
                             <label>Nom d'utilisateur</label>
                             <i class="form-group__bar"></i>
                         </div>
+                        <p><span id="userspan"></span></p>
 
                         <div class="form-group form-group--float form-group--centered">
-                            <input type="password" class="form-control" name="mdp">
+                            <input type="password" class="form-control" name="mdp" id="mdp">
                             <label>Password</label>
                             <i class="form-group__bar"></i>
                         </div>                  
+                        <p><span id="mdpspan"></span></p>
+
                         <input type="text" value="signup" name="signup" hidden>
-                        <button type="submit" class="btn btn--icon login__block__btn"><i class="zmdi zmdi-check"></i></button>
+                        <button type="submit" id="Subtn" class="btn btn--icon login__block__btn"><i class="zmdi zmdi-check"></i></button>
                     </form>
                 </div>
             </div>        
@@ -57,42 +62,69 @@
     //=====================validation formulaire de login=======================
       
     var form = document.getElementById("form");
-    var username1 = document.getElementById("username1");
-    var mdp1 = document.getElementById("mdp1");
-    var loginSub = document.getElementById("loginSub");    
+    var username = document.getElementById("username");
     var userspan = document.getElementById("userspan");
-    var mdpspan = document.getElementById("mdpspan");   
-
-    loginSub.addEventListener("click",function(e){
+    var mdp = document.getElementById("mdp");
+    var mdpspan = document.getElementById("mdpspan"); 
+    var nom = document.getElementById("nom");
+    var namespan = document.getElementById("namespan");
+    var place = document.getElementById("place");
+    var placespan = document.getElementById("placespan"); 
+    var Subtn = document.getElementById("Subtn");  
+    
+    Subtn.addEventListener("click",function(e){
         e.preventDefault();
         
-        if(username1.value.length <= 0 || mdp1.value.length <= 0){    
+        if(username.value.length <= 0 || mdp.value.length <= 0 ||
+        nom.value.length <= 0 || place.value.length <= 0){    
                   
-            if(mdp1.value.length <= 0){
-                mdp1.setAttribute("style","border-color:red;transition:2s");
+            if(mdp.value.length <= 0){
+                mdp.setAttribute("style","border-color:red;transition:2s");
                 mdpspan.innerHTML = "Le mot de passe est requis";
                 mdpspan.setAttribute("style","color:red;transition:1s;")
                 console.log("mdp pass pas");
             }
-            if(username1.value.length <= 0){
-                username1.setAttribute("style","border-color:red;transition:1s")
+            if(username.value.length <= 0){
+                username.setAttribute("style","border-color:red;transition:1s")
                 userspan.innerHTML = "Le nom d'utilisateur est requis";
                 userspan.setAttribute("style","color:red;transition:2s;");
                 console.log("username pass pas");
             }
+            if(place.value.length <= 0){
+                place.setAttribute("style","border-color:red;transition:1s")
+                placespan.innerHTML = "Le lieu de consultation est requis";
+                placespan.setAttribute("style","color:red;transition:2s;");
+                console.log("place pass pas");
+            }
+            if(nom.value.length <= 0){
+                nom.setAttribute("style","border-color:red;transition:1s")
+                namespan.innerHTML = "Le nom complet est requis";
+                namespan.setAttribute("style","color:red;transition:2s;");
+                console.log("name pass pas");
+            }
+           
         }
         else{
             form.submit();
         }        
     });
 
-    username1.addEventListener("click",function(e){     
-        username1.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");   
+    username.addEventListener("click",function(e){     
+        username.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");   
         userspan.innerHTML = "";        
     });
 
-    mdp1.addEventListener("click",function(e){
-        mdp1.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");
+    mdp.addEventListener("click",function(e){
+        mdp.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");
         mdpspan.innerHTML = "";        
+    });
+    nom.addEventListener("click",function(e){     
+        nom.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");   
+        namespan.innerHTML = "";        
+    });
+
+    place.addEventListener("click",function(e){
+        place.setAttribute("style","border-color:rgba(0,0,50,0.1);transition:2s");
+        placespan.innerHTML = "";        
     });
 </script>
