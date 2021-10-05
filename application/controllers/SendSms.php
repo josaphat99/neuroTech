abbe63c24f8e4267f80c68deb3ac6b1e60d368bd
elisee <elisee.kalonji99@gmail.com>
2021-10-05 07:47:35 +0200

    ready to push v1

--- application/controllers/SendSms.php
+++ application/controllers/SendSms.php
@@ -1,34 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require __DIR__ . '/vendor/autoload.php';
-use Twilio\Rest\Client;
+// require __DIR__ . '/vendor/autoload.php';
+// use Twilio\Rest\Client;
 
-class SendSms
-{
-    //SMS sending  
-    public function send_sms($account_sid,$auth_token,$twilio_number,$receiver,$message)
-    {
-        // Your Account SID and Auth Token from twilio.com/console
-        $account_sid = $account_sid;
-        $auth_token = $auth_token;
+// class SendSms
+// {
+//     //SMS sending  
+//     public function send_sms($account_sid,$auth_token,$twilio_number,$receiver,$message)
+//     {
+//         // Your Account SID and Auth Token from twilio.com/console
+//         $account_sid = $account_sid;
+//         $auth_token = $auth_token;
 
-        /* 
-        In production, these should be environment variables. E.g.:
-        $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
-        */
+//         /* 
+//         In production, these should be environment variables. E.g.:
+//         $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+//         */
 
-        // A Twilio number you own with SMS capabilities
-        $twilio_number = $twilio_number;
+//         // A Twilio number you own with SMS capabilities
+//         $twilio_number = $twilio_number;
 
-        $client = new Client($account_sid, $auth_token);
+//         $client = new Client($account_sid, $auth_token);
 
-        $client->messages->create(            
-            $receiver, // Where to send a text message (your cell phone?)
-            array(
-                'from' => $twilio_number,
-                'body' => $message
-            )
-        );
-    }    
-}
+//         $client->messages->create(            
+//             $receiver, // Where to send a text message (your cell phone?)
+//             array(
+//                 'from' => $twilio_number,
+//                 'body' => $message
+//             )
+//         );
+//     }    
+// }
